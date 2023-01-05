36162f392466f433ed587dd96914d21dcb7ea813
unknown <sayan.babu.dutta@gmail.com>
2023-01-05 13:31:34 +0530

    new if else clause added

--- newlearn.py
+++ newlearn.py
@@ -1,1 +1,5 @@
-print('Hello I am learning git.')
+ans=input('Hello, Are you learning git?')
+if ans=="yes":
+    print('Best wishes to learn git.')
+else:
+    print('Hurry, you need to learn git.')
